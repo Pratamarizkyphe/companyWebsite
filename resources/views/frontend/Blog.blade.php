567b4fc3417e1cc58f2291e323b92f5ad9d38b9b
Pratamarizkyphe <pratamarizky249b@gmail.com>
2022-01-31 10:03:46 +0700

    memperbaiki footer

--- resources/views/frontend/Blog.blade.php
+++ resources/views/frontend/Blog.blade.php
@@ -88,65 +88,43 @@
       ">
       </div>
     
- @foreach ($footer as $foot)
-     
-  
- <footer class="footer bg-cover bg-no-repeat bg-right-top pt-120 mt-25 bg-theme-color md:bg-transparent md:pt-260 lg:pt-260" style="background-image: url('img/footer/footer-bg.svg');">
-  <div class="container">
-    <div class="widget-wrapper">
-    
-        <div class="row">
-          
-          <div class="w-full md:w-6/12 lg:w-4/12">
-            <div class="footer-widget mb-10 mx-3">
-              <div data-aos="zoom-in-down" 
-                  >
-                <div class="logo mb-8">
-                  <a href="index.html"> <img src="img/logo/logo.svg" alt=""> </a>
-                </div>
-                <p class="mb-8 text-white sm:pr-13 md:pr-0 2xl:pr-25">{{$foot->text}}.</p>
-              </div>
-            </div>
+      <div class="mt-16">
+        <footer class=" body-font bg-cover bg-right-top " style="background-image: url('img/footer/footer-bg.svg');">
+          <div class=" px-25 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+           
           </div>
-
-          <div class="w-full md:w-6/12 lg:w-2/12">
-            <div class="footer-widget mb-10 mx-3">
-              <div data-aos="zoom-in-up">
-                <h3 class="mb-6 text-white">About Us</h3>
-                <ul class="links">
-                  <li> <a href="/">Home</a> </li>
-                  <li> <a href="/gallery">Gallery</a> </li>
-                  <li> <a href="/blog">Blog</a> </li>
-                  <li> <a href="/portfolio">Portfolio</a> </li>
-                  <li> <a href="/contact">Contact</a> </li>
-                  <li> <a href="/profile">Profile</a> </li>
-                </ul>
-              </div>
+          <div class=" flex flex-wrap md:pl-20 -mb-10 md:mt-20 mt-50 md:text-left text-center">
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <div data-aos="zoom-in-up">
+              <h3 class="mb-6 text-white">About Us</h3>
+              <ul class="links">
+                <li> <a href="/">Home</a> </li>
+                <li> <a href="/gallery">Gallery</a> </li>
+                <li> <a href="/blog">Blog</a> </li>
+                <li> <a href="/portfolio">Portfolio</a> </li>
+                <li> <a href="/contact">Contact</a> </li>
+                <li> <a href="/profile">Profile</a> </li>
+              </ul>
             </div>
-          </div>
-
-          <div class="w-full md:w-6/12 lg:w-3/12">
-            <div class="footer-widget mb-10 mx-3">
-              <div data-aos="zoom-in-up"
-                  >
-                <h3 class="mb-6 text-white">Contact Us</h3>
-                <ul class="links">
-                  <li> <a href="#">+62 8821 4826 417</a> </li>
-                  <li> <a href="#">pratamarizky249b@gmail.com</a> </li>
-                  <li> <a href="#">Kebakkramat, Karanganyar</a> </li>
-                  
-                </ul>
-              </div>
             </div>
-          </div>
-
-          
-
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+            <div data-aos="zoom-in-up"
+            >
+          <h3 class="mb-6 text-white">Contact Us</h3>
+          <ul class="links">
+            <li> <a href="#">+62 8821 4826 417</a> </li>
+            <li> <a href="#">pratamarizky249b@gmail.com</a> </li>
+            <li> <a href="#">Kebakkramat, Karanganyar</a> </li>
+            
+          </ul>
         </div>
-      
-    </div>
-
-  </div>
-</footer>
-    @endforeach  
+            </div>
+           
+          </div>
+          </div>
+    
+        </footer>
+        </div>
+        
         @endsection
